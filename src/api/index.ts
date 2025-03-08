160840a1295a9f67a086e7292e372862f4cd7c02
孙一航 <123456@163com>
2025-03-08 09:16:16 +0800

    删除文件

--- src/api/index.ts
+++
@@ -1,9 +0,0 @@
-import request from '@/utils/request'
-
-export const getData = <T>(url: string, data: any = {}) => {
-  return request<T>(url, 'POST', data)
-}
-
-export const postData = <T>(url: string, data: any) => {
-  return request<T>(url, 'POST', data)
-}
--- src/api/user.ts
+++
@@ -1,6 +0,0 @@
-import request from '@/utils/request'
-
-/** 登录账号 */
-export const loginAPI = <T>(url: string, data: any) => {
-  return request<T>(url, 'POST', data)
-}
